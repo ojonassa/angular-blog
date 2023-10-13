cbc4fda259f135f07d1b7cdb88fb0e7c8879ebf6
jonas <jonassaraivasantos@gmail.com>
2023-10-13 12:12:45 -0300

    Finalizado a dinamização dos conteúdos

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "AUTO DA COMPADECIDA 2",
+    "description": "Sequência da minissérie de 1999, com Matheus Nachtergaele e Selton Mello, está prevista para estrear em 2024. \br  \br \br \br Anunciado em março deste ano, O Auto da Compadecida 2, a tão desejada sequência da minissérie de sucesso de 1999, vai finalmente acontecer. Com Matheus Nachtergaele e Selton Mello novamente como a icônica dupla João Grilo e Chicó, o filme está previsto para estrear em 2024 e, a seguir, o CineBuzz lista tudo o que já sabemos sobre a novidade. Qual é a história? Além do reencontro de João Grilo e Chicó, após 25 anos, pouco sabemos sobre o enredo de O Auto da Compadecida 2 até o momento, mas sabemos que ela foi apresentada à família de Ariano Suassuna, autor da obra que inspirou o capítulo anterior, e aprovada antes de ser produzida. Em entrevista ao G1, Manuel Dantas Suassuna, filho do autor, revelou que, logo após a estreia de O Auto da Compadecida, Guel Arraes, diretor da minissérie, chegou a conversar com Suassuna sobre a possibilidade de uma sequência, mas os planos acabaram não indo em frente.",
+    "photoCover": "https://s2-quem.glbimg.com/KQ4KmY-4GMRUIYww6kmee7jdWFU=/0x0:1080x1350/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_b0f0e84207c948ab8b8777be5a6a4395/internal_photos/bs/2023/K/I/3O9C1OTqODAbAH1jOYcQ/snapinsta.app-1080-334684984-594615066028210-4973006811210157800-n-1-.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Cidade de Deus - Incrível filme Brasileiro retrata a realidade da favela do Rio de Janeiro",
+    "description": "A maior obra prima do cinema nacional !!! Um dos melhores filmes da História !!! A realidade nua e crua das favelas brasileiras. A direção , fotografia e atuações são impecáveis. O fato dos atores do filme serem pessoas da comunidade , torna tudo tão natural , às vezes , parece até um documentário , tu se sente dentro do local. O roteiro é um dos melhores do cinema , frenético , genial , o modo de contar a história com pequenas histórias e flashbacks não é confuso. É tudo bem amarrado e coerente. Todos os personagens são memoráveis. Cada um é tão bem desenvolvido , com camadas e personalidades. Tenho muitas cenas favoritas como a da perseguição da galinha , a morte do Cabeleira ,  Dadinho é o caralh0 !!! Meu nome agora é Zé Pequeno , porr* !!!  , Quer o tiro no pé ou na mão ?  , a morte do Bené , o tiroteio no final ... Mas em geral , todas são incríveis. Vale a pena também falar da trilha sonora , que vai desde de um samba do Cartola até uma discoteca dos Anos 70. Quem fala que a violência do filme é exagerada , não sabe do que tá falando ... Esse clássico é referência mundial , muitos diretores estrangeiros são fãs e se inspiram nele. Devia ter ganhado o Oscar de Melhor Filme Estrangeiro em 2003 , mas foi boicotado ( inveja , talvez ? ) , uma injustiça. Poderia passar horas falando desse filme maravilhoso e icônico.",
+    "photoCover": "https://m.media-amazon.com/images/I/711ao27c6TL._AC_UF1000,1000_QL80_.jpg"
+
+  }
+  ,
+  {
+    "id": "3",
+    "title": "Novo PES 2024 acaba de ser lançado, já englobando as ultimas contrações.",
+    "description": "A Konami lançou no fim da última semana uma atualização de mudança de temporada no antigo PES, que agora se chama eFootball 2024. De todas as atualizações recentes do título desde a mudança da franquia PES, essa foi, de longe, a mais bem recebida pela comunidade. Apesar dos variados modos de jogo que popularizaram a franquia, como a Master League e o modo carreira, ainda estarem de fora, a edição da próxima temporada trouxe a maior evolução tanto técnica como gráfica para o jogo, principalmente considerando a estreia desastrosa da nova série no lançamento do eFootball 2022.",
+    "photoCover": "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltdc7650aceaf239ab/64fb7b8109b7b6cd069930a2/efootball2024_mainvisual_messi.jpg?auto=webp&format=pjpg&width=3840&quality=60"
+
+  },
+  {
+    "id": "4",
+    "title": "SOUND OF FREEDOM - Som da Liberdade",
+    "description": "Sonho de Liberdade é como um raio de sol no mundo do cinema! Este filme de 1994 é uma jornada emocionante e inspiradora que vai além das grades da prisão. Vamos dar uma olhada em suas qualidades brilhantes: Alegria na Adversidade: Mesmo ambientado em uma prisão, o filme transmite uma mensagem poderosa de esperança e perseverança. A história de Andy Dufresne é um testemunho de como a alegria pode florescer em meio à adversidade. Amizade Duradoura: A amizade entre Andy (interpretado por Tim Robbins) e Red (interpretado por Morgan Freeman) é como um abraço caloroso. Suas interações divertidas e sinceras proporcionam momentos leves, equilibrando o ambiente pesado da prisão. Humor Sutil: Apesar das circunstâncias difíceis, o filme apresenta um toque de humor sutil que faz você sorrir. Os diálogos inteligentes e as situações inusitadas adicionam uma camada de leveza à trama. Lições de Vida: Sonho de Liberdade não é apenas sobre escapar de uma prisão física, mas também sobre se libertar das prisões emocionais. As lições de vida oferecidas ao longo do filme são como pequenas pepitas de ouro. Cinematografia Encantadora: A cinematografia de Roger Deakins pinta quadros deslumbrantes, destacando a beleza mesmo nos lugares mais inesperados. Cada cena é uma obra-prima visual que pode iluminar até mesmo os dias mais nublados. Trilha Sonora Envolvente: A trilha sonora de Thomas Newman é uma sinfonia de emoções positivas. Ela complementa perfeitamente a jornada de Andy e Red, elevando o espírito do espectador. Impacto Duradouro: Não é à toa que Sonho de Liberdade conquistou corações ao redor do mundo. Sua mensagem atemporal continua a inspirar pessoas a encontrarem alegria, mesmo nas situações mais desafiadoras. Então, se você está em busca de uma experiência cinematográfica que aquece o coração e deixa um sorriso no rosto, Sonho de Liberdade é a escolha perfeita. Este clássico atemporal é como um abraço cinéfilo cheio de alegria e otimismo!",
+    "photoCover": "https://jpimg.com.br/uploads/2023/08/soundfreedom_hiperionblog-1024x658.jpg"
+
+  }
+]
+
+
